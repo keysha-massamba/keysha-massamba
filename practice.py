b16c043b6dc709c72bdfd9260d22299f6e0ac0d5
Your Name <K.Massamba3576@student.leedsbeckett.ac.uk>
2026-01-15 12:17:44 +0000

    practice

---
+++ practice.py
@@ -0,0 +1,20 @@
+print("hello keysha")
+
+name = input("what is your name?")
+print("hello" + name )
+
+# challenge notebook 2 ask user for a number and print the square of that number
+number = int(input ("enter a number:"))
+squared_number = number * number
+print (squared_number)
+
+#challenge 2 cricket average !!
+
+#challenge 3 area of pizza !!!
+import math 
+diameter_inch = 10 
+radius_inch = diameter_inch / 2
+area_inch = math.pi * radius_inch * radius_inch
+print("area of pizza in square inches:" , area_inch)
+
+
